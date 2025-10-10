7836ce8c92af87203fe299f9f908795fc2af2c1f
Luca Tramonti <luca.tramonti@outlook.it>
2025-10-10 20:58:33 +0200

    migliorie

--- utils.py
+++ utils.py
@@ -17,6 +17,10 @@ SOGLIA_BONUS_RENZI = 28_000.0
 
 SOGLIE_DET_LAV_DIP = (15_000.0, 28_000.0, 50_000.0)
 
+DETRAZIONE_MASSIMA_FONDO = 5_164.57
+SOGLIA_FONDO_LAVORATORE = 0.0055
+QUOTA_FONDO_DATORE = 0.0155
+
 
 def calcola_contributi(ral: float, aliquota_contributiva: float = ALIQUOTA_CONTRIBUTIVA) -> float:
     return ral * aliquota_contributiva
@@ -54,11 +58,49 @@ def detrazioni_irpef_lavoro_dipendente(imponibile: float) -> float:
         return 1910.0 * (SOGLIE_DET_LAV_DIP[2] - imponibile) / (SOGLIE_DET_LAV_DIP[2] - SOGLIE_DET_LAV_DIP[1])
 
 
+def versato_nel_fondo(ral: float, quota_lavoratore: float, soglia_lavoratore: float = SOGLIA_FONDO_LAVORATORE, quota_datore: float = QUOTA_FONDO_DATORE) -> float:
+    if quota_lavoratore / ral < soglia_lavoratore:
+        return quota_lavoratore
+    else:
+        return quota_lavoratore + ral * quota_datore
+
+
+def calcola_imponibile(ral: float, versamento_fondo: float, deduzioni_extra: float = 0.0) -> float:
+    result = ral
+    # Togli contributi e deduzioni extra
+    result = result - calcola_contributi(ral) - deduzioni_extra
+    # Togli deduzioni del fondo pensione
+    quota_datore = versato_nel_fondo(ral, versamento_fondo) - versamento_fondo
+    result = result - min(versamento_fondo, DETRAZIONE_MASSIMA_FONDO-quota_datore)
+    return result
+
+
+def calcola_irpef_netta(imponibile: float, detrazioni_extra: float = 0.0) -> float:
+    result = calcola_irpef_lordo(imponibile)
+    # Togli detrazioni da lavoro dipendente
+    result = result - detrazioni_irpef_lavoro_dipendente(imponibile)
+    # Togli detrazioni extra (bisogna essere capienti)
+    result = max(0.0, result - detrazioni_extra)
+    # Togli il bonus Renzi
+    if imponibile < SOGLIA_BONUS_RENZI:
+        result = max(0.0, result - MAX_BONUS_RENZI)
+    return result
+
+
+def calcola_netto_annuo(ral: float,
+                        versamenti_fondo: float,
+                        deduzioni_extra: float = 0,
+                        detrazioni_extra: float = 0,
+                        ) -> float:
+    imponibile = calcola_imponibile(ral, versamenti_fondo, deduzioni_extra)
+    totale_irpef = (calcola_irpef_netta(imponibile, detrazioni_extra) +
+                    calcola_addizionale_regionale(imponibile) +
+                    calcola_addizionale_comunale(imponibile))
+    netto = ral - calcola_contributi(ral) - versamenti_fondo - totale_irpef
+    return netto
 
 if __name__ == '__main__':
-    imponibile = 40000
-    print(calcola_irpef_lordo(imponibile))
-    print(calcola_addizionale_regionale(imponibile))
-    print(calcola_addizionale_comunale(imponibile))
-    print(detrazioni_irpef_lavoro_dipendente(imponibile))
+    print(calcola_netto_annuo(25000, 4500))
+    print(calcola_netto_annuo(25000, 5000))
+    print(calcola_netto_annuo(25000, 10000))
 
